ccf3e45080fed5c12a5b1cea1b63792b0b134c6c
AinoMegumi <aimegu2014@gmail.com>
2020-04-19 02:11:44 +0900

    仮コードの完成(この状態ではビルドエラー)

--- Server/ResourceAccessServer.cpp
+++ Server/ResourceAccessServer.cpp
@@ -1,68 +1,116 @@
 ﻿#include "ResourceAccessServer.hpp"
+#include "ServiceStatus.h"
+#include "Split.hpp"
 #include <picojson.h>
-#include <Windows.h>
 #include <Pdh.h>
 #include <Psapi.h>
-#include <vector>
 #include <filesystem>
-constexpr size_t ArrayBufferSize = 1024;
+#include <unordered_map>
 #pragma comment(lib, "pdh.lib")
 
 ServiceProcess* GetServiceProcessInstance(const Service_CommandLineManager::CommandLineType& args) {
 	return new ResourceAccessServer(args);
 }
 
+inline std::unordered_map<std::string, Disk> GetDiskResourceInformations(const IniRead& ini) {
+	std::unordered_map<std::string, Disk> RetVal{};
+	const std::vector<std::string> DriveList = SplitString(ini.GetString("resource", "drives", "C:"), ',');
+	for (const auto& i : DriveList) {
+		try {
+			RetVal.emplace(std::make_pair(i, Disk(i)));
+		}
+		catch(std::exception&) { // ないドライブの時にエラー起こすのでここでcatch
+			
+		}
 	}
-inline std::string GetAllResourceData(const Processor& p, const MemoryManager& m, const Disk& d, const Network& n) {
-	jsonobject obj{};
-	obj.insert("cpu", p.Get());
-	obj.insert("memory", m.Get());
-	obj.insert("disk", d.Get());
-	obj.insert("network", n.Get());
-	return picojson::value(obj).to_str();
 }
 
 ResourceAccessServer::ResourceAccessServer(const Service_CommandLineManager::CommandLineType& args)
-	: ServiceProcess(args), server() {}
+	: ServiceProcess(args), server() {
+	SvcStatus.dwControlsAccepted = SERVICE_ACCEPT_SHUTDOWN | SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_PAUSE_CONTINUE;
+	SetServiceStatusInfo();
+}
 
 void ResourceAccessServer::Service_MainProcess() {
+	// using
+
+	using Req = const httplib::Request&;
+	using Res = httplib::Response&;
+
+	// 変数とラムダ
+
 	IniRead ini(BaseClass::ChangeFullPath(".\\server.ini"));
+	auto GetConfStr = [&ini](const std::string& Section, const std::string& Key, const std::string& Default) { return ini.GetString(Section, Key, Default).c_str(); };
+	auto GetConfInt = [&ini](const std::string& Section, const std::string& Key, const int& Default) { return ini.GetNum(Section, Key, Default); };
 	Processor processor{};
-	MemoryManager memory{};
-	Disk disk(ini.GetString("resource", "drive", "C:"));
-	Network network(ini.GetString("resource", "network", "Realtek PCIe GBE Family Controller"));
-	this->server.Get(ini.GetString("url", "all", "/v1/").c_str(),
-		[&](const httplib::Request& req, httplib::Response& res) {
+	Memory memory{};
+	std::unordered_map<std::string, Disk> disk = GetDiskResourceInformations(ini);
+	Network network(GetConfStr("system", "netdevice", "Realtek PCIe GBE Family Controller"));
+	auto reqproc = [](Res res, const std::function<void()>& process) {
+		try { process(); }
+		catch (...) { res.status = 500; }
+	};
+	auto ChangeSvcStatus = [](const DWORD NewStatus) {
+		SvcStatus.dwCurrentState = NewStatus;
+		SetServiceStatusInfo();
+	};
+	auto alljsonobj = [&]() {
+		JsonObject obj{};
+		JsonObject diskinfo{};
+		obj.insert("cpu", processor.Get());
+		obj.insert("memory", memory.Get());
+		for (const auto& i : disk) diskinfo.insert(i.first.substr(0, 1), i.second.Get());
+		obj.insert("disk", diskinfo);
+		obj.insert("network", network.Get());
+		return obj;
+	};
+	auto json = [](const picojson::object& obj) { return picojson::value(obj).to_str(); };
 
+	// メインプロセス
+	while (SvcStatus.dwCurrentState != SERVICE_STOP_PENDING) {
+		if (SvcStatus.dwCurrentState == SERVICE_PAUSED)	Sleep(1000);
+		else {
+			this->server.Get(GetConfStr("url", "all", "/v1/"), [&](Req, Res res) { reqproc(res, [&] { res.set_content(json(alljsonobj()), "text/json"); }); });
+			this->server.Get(GetConfStr("url", "cpu", "/v1/cpu"), [&](Req, Res res) { reqproc(res, [&] { res.set_content(json(processor.Get()), "text/json"); }); });
+			this->server.Get(GetConfStr("url", "memory", "/v1/mem"), [&](Req, Res res) { reqproc(res, [&] { res.set_content(json(memory.Get()), "text/json"); }); });
+			this->server.Get(GetConfStr("url", "storage", "/v1/disk/[A-Z]"),
+				[&](Req req, Res res) {
+					reqproc(res, 
+						[&] {
+							if (const std::string drive = req.matches[1].str() + ":"; disk.find(drive) == disk.end()) res.status = 404;
+							else res.set_content(json(disk.at(drive).Get()), "text/json");
+						}
+					);
+				}
+			);
+			this->server.Get(GetConfStr("url", "network", "/v1/network"), [&](Req, Res res) { reqproc(res, [&] { res.set_content(json(network.Get()), "text/json"); }); });
+			this->server.listen(GetConfStr("url", "domain", "localhost"), GetConfInt("url", "port", 8080), 0,
+				[&]{
+					switch (SvcStatus.dwCurrentState) {
+						case SERVICE_START_PENDING:
+						case SERVICE_CONTINUE_PENDING:
+							processor.Update();
+							memory.Update();
+							for (const auto& i : disk) i.second.Update();
+							network.Update();
+							ChangeSvcStatus(SERVICE_RUNNING);
+							break;
+						case SERVICE_RUNNING:
+							processor.Update();
+							memory.Update();
+							for (const auto& i : disk) i.second.Update();
+							network.Update();
+							break;
+						case SERVICE_STOP_PENDING:
+						case SERVICE_PAUSE_PENDING:
+							this->server.stop();
+							break;
+					}
+				}
+			);	
+			if (SvcStatus.dwCurrentState == SERVICE_PAUSE_PENDING) ChangeSvcStatus(SERVICE_PAUSED);
 		}
-	);
-	this->server.Get(ini.GetString("url", "cpu", "/v1/cpu").c_str(),
-		[&](const httplib::Request& req, httplib::Response& res) {
-
-		}
-	);
-	this->server.Get(ini.GetString("url", "memory", "/v1/mem").c_str(),
-		[&](const httplib::Request& req, httplib::Response& res) {
-
-		}
-	);
-	this->server.Get(ini.GetString("url", "storage", "/v1/disk/[A-Z]").c_str(),
-		[&](const httplib::Request& req, httplib::Response& res) {
-			const std::string drive = req.matches[1].str() + ":";
-
-		}
-	);
-	this->server.Get(ini.GetString("url", "network", "/v1/network").c_str(),
-		[&](const httplib::Request& req, httplib::Response& res) {
-
-		}
-	);
-	this->server.listen(ini.GetString("url", "domain", "localhost").c_str(), ini.GetNum("url", "port", 8080), 0,
-		[&]() {
-			processor.Update();
-			memory.Update();
-			disk.Update();
-			network.Update();
-		}
-	);
+	}
+	SvcStatus.dwCurrentState = SERVICE_STOPPED;
+	SetServiceStatusInfo();
 }
---
+++ Server/Split.hpp
@@ -0,0 +1,44 @@
+﻿#ifndef __SPLIT_HPP__
+#define __SPLIT_HPP__
+#include <string>
+#include <vector>
+#include <array>
+#include <sstream>
+#include <type_traits>
+#include <algorithm>
+
+template<typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::vector<std::basic_string<T>> SplitString(const std::basic_string<T> &str, const T sep) {
+	std::vector<std::basic_string<T>> Arr;
+	std::basic_string<T> Item;
+	for (T i : str) {
+		if (i == sep) {
+			if (!Item.empty()) Arr.push_back(Item);
+			Item.clear();
+		}
+		else Item += i;
+	}
+	if (!Item.empty()) Arr.push_back(Item);
+	return Arr;
+}
+
+template<typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::vector<std::basic_string<T>> SplitString(const T* str, const T sep) {
+	return SplitString(std::basic_string<T>(str), sep);
+}
+
+template<size_t ArrSize, typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::array<std::basic_string<T>, ArrSize> SplitString(const std::basic_string<T>& str, const T sep) {
+	const std::vector<std::basic_string<T>> List = SplitString<T>(str, sep);
+	if (List.size() > ArrSize) throw std::runtime_error("Split data is over.");
+	std::array<std::basic_string<T>, ArrSize> RetData;
+	RetData.fill(T{});
+	for (size_t i = 0; i < std::min(ArrSize, List.size()); i++) RetData[i] = std::move(List[i]);
+	return RetData;
+}
+
+template<size_t ArrSize, typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::array<std::basic_string<T>, ArrSize> SplitString(const T*& str, const T sep) {
+	return SplitString(std::basic_string<T>(str), sep);
+}
+#endif
