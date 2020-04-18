d2eadfab54e788face0681309f8986b82171b9b4
AinoMegumi <aimegu2014@gmail.com>
2020-04-19 01:42:55 +0900

    PDHCounterをResourceAccessServer.cppから移動

---
+++ Server/PDHCounter.cpp
@@ -0,0 +1,37 @@
+﻿#include "PDHCounter.hpp"
+#include "PDHAssistFunctions.hpp"
+#include <stdexcept>
+
+PDHCounter::PDHCounter(const std::string& CategoryName, const std::string& CounterName, const std::string& instanceName)
+	: CategoryName(CategoryName), CounterName(CounterName), instanceName(instanceName) {
+	if (PdhOpenQuery(NULL, NULL, &this->hQuery) != ERROR_SUCCESS) throw std::runtime_error("Failed to open PDH Query");
+	PdhAddCounterA(this->hQuery, ("\\" + CategoryName + "(" + instanceName + ")\\" + CounterName).c_str(), NULL, &this->hCounter);
+}
+
+PDHCounter::PDHCounter(const std::string& CategoryName, const std::string& CounterName)
+	: CategoryName(CategoryName), CounterName(CounterName), instanceName() {
+	if (PdhOpenQuery(NULL, NULL, &this->hQuery) != ERROR_SUCCESS) throw std::runtime_error("Failed to open PDH Query");
+	PdhAddCounterA(this->hQuery, ("\\" + CategoryName + "\\" + CounterName).c_str(), NULL, &this->hCounter);
+}
+
+void PDHCounter::Update() const {
+	PdhCollectQueryData(this->hQuery);
+}
+
+double PDHCounter::GetDoubleValue() const {
+	PDH_FMT_COUNTERVALUE val{};
+	PdhGetFormattedCounterValue(this->hCounter, PDH_FMT_DOUBLE, NULL, &val);
+	return val.doubleValue;
+}
+
+LONGLONG PDHCounter::GetInt64Value() const {
+	PDH_FMT_COUNTERVALUE val{};
+	PdhGetFormattedCounterValue(this->hCounter, PDH_FMT_LARGE, NULL, &val);
+	return val.largeValue;
+}
+
+long PDHCounter::GetLongValue() const {
+	PDH_FMT_COUNTERVALUE val{};
+	PdhGetFormattedCounterValue(this->hCounter, PDH_FMT_LARGE, NULL, &val);
+	return val.longValue;
+}
---
+++ Server/PDHCounter.hpp
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <Pdh.h>
+#include <string>
+
+class PDHCounter {
+public:
+	std::string CategoryName;
+	std::string CounterName;
+	std::string instanceName;
+private:
+	PDH_HQUERY hQuery;
+	PDH_HCOUNTER hCounter;
+public:
+	PDHCounter(const std::string& CategoryName, const std::string& CounterName, const std::string& instanceName)
+		: CategoryName(CategoryName), CounterName(CounterName), instanceName(instanceName) {
+		if (PdhOpenQuery(NULL, NULL, &this->hQuery) != ERROR_SUCCESS) throw std::runtime_error("Failed to open PDH Query");
+		PdhAddCounterA(this->hQuery, ("\\" + CategoryName + "(" + instanceName + ")\\" + CounterName).c_str(), NULL, &this->hCounter);
+	}
+	PDHCounter(const std::string& CategoryName, const std::string& CounterName)
+		: CategoryName(CategoryName), CounterName(CounterName), instanceName() {
+		if (PdhOpenQuery(NULL, NULL, &this->hQuery) != ERROR_SUCCESS) throw std::runtime_error("Failed to open PDH Query");
+		PdhAddCounterA(this->hQuery, ("\\" + CategoryName + "\\" + CounterName).c_str(), NULL, &this->hCounter);
+	}
+	void Update() const {
+		PdhCollectQueryData(this->hQuery);
+	}
+	double GetDoubleValue() const {
+		PDH_FMT_COUNTERVALUE val{};
+		PdhGetFormattedCounterValue(this->hCounter, PDH_FMT_DOUBLE, NULL, &val);
+		return digit(val.doubleValue);
+	}
+	LONGLONG GetInt64Value() const {
+		PDH_FMT_COUNTERVALUE val{};
+		PdhGetFormattedCounterValue(this->hCounter, PDH_FMT_LARGE, NULL, &val);
+		return val.largeValue;
+	}
+	long GetLongValue() const {
+		PDH_FMT_COUNTERVALUE val{};
+		PdhGetFormattedCounterValue(this->hCounter, PDH_FMT_LARGE, NULL, &val);
+		return val.longValue;
+	}
+};
