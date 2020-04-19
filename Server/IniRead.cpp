9c48740d872faf995aa8b1c84200528d122d32e2
AinoMegumi <aimegu2014@gmail.com>
2020-04-19 13:53:52 +0900

    IniReadのGetStringを修正

--- Server/IniRead.cpp
+++ Server/IniRead.cpp
@@ -8,5 +8,9 @@ int IniRead::GetNum(const std::string& Section, const std::string& Key, const in
 std::string IniRead::GetString(const std::string& Section, const std::string& Key, const std::string& Default) const {
 	char Buffer[ArrayBufferSize];
 	GetPrivateProfileStringA(Section.c_str(), Key.c_str(), Default.c_str(), Buffer, ArrayBufferSize, this->IniFilePath.c_str());
-	return std::string(Buffer);
+	std::string str{};
+	str.reserve(ArrayBufferSize);
+	str = Buffer;
+	str.resize(std::strlen(str.c_str()));
+	return str;
 }
