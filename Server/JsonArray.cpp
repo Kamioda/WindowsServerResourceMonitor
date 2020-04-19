b671cc855924102f89ed7806914ed37b9800ad51
AinoMegumi <aimegu2014@gmail.com>
2020-04-19 19:50:54 +0900

    JsonArrayクラスを作成

---
+++ Server/JsonArray.cpp
@@ -0,0 +1,7 @@
+﻿#include "JsonArray.hpp"
+
+void JsonArray::insert(const picojson::object& obj) {
+	this->arr.push_back(picojson::value(obj));
+}
+
+JsonArray::operator const picojson::array& () const noexcept { return this->arr; }
---
+++ Server/JsonArray.hpp
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <picojson.h>
+
+class JsonArray {
+private:
+	picojson::array arr;
+public:
+	JsonArray() = default;
+	void insert(const picojson::object& obj);
+	operator const picojson::array& () const noexcept;
+};
