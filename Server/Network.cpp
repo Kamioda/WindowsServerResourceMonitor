8253e46ec5820e4aad1bcaac6218c412cf0b24cd
AinoMegumi <aimegu2014@gmail.com>
2020-04-19 19:39:59 +0900

    変数名を修正

--- Server/Network.cpp
+++ Server/Network.cpp
@@ -11,7 +11,7 @@ namespace impl {
 }
 
 Network::Network(const std::string& NetworkDeviceName)
-	: NetworkDeviceName(NetworkDeviceName), netReceive(NetworkDeviceName), netSend(NetworkDeviceName) {}
+	: DeviceName(NetworkDeviceName), netReceive(NetworkDeviceName), netSend(NetworkDeviceName) {}
 
 void Network::Update() const {
 	this->netReceive.Update();
@@ -20,7 +20,7 @@ void Network::Update() const {
 
 picojson::object Network::Get() const {
 	JsonObject obj{};
-	obj.insert("device", this->NetworkDeviceName);
+	obj.insert("device", this->DeviceName);
 	obj.insert("receive", this->netReceive.Get());
 	obj.insert("send", this->netSend.Get());
 	return obj;
