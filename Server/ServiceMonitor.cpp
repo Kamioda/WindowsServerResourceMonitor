a501a377ca252efc1acab066608fc2886aebf89f
AinoMegumi <aimegu2014@gmail.com>
2020-04-20 10:26:33 +0900

    未サポートステータスとサービスの種類についてサポート開始に合わせてコードを変更

--- Server/ServiceMonitor.cpp
+++ Server/ServiceMonitor.cpp
@@ -2,11 +2,23 @@
 
 std::unordered_map<DWORD, std::string> ServiceMonitor::StatusList;
 
+std::unordered_map<DWORD, std::string> ServiceMonitor::ServiceTypeList;
+
 void ServiceMonitor::InitStatusList(const IniRead& ini) {
-	const DWORD ServiceStatusList[7] = { SERVICE_RUNNING, SERVICE_STOPPED, SERVICE_PAUSED, SERVICE_START_PENDING, SERVICE_PAUSE_PENDING, SERVICE_CONTINUE_PENDING, SERVICE_STOP_PENDING };
-	const std::string ServiceStatusLoadKeyList[7] = { "running", "stopped", "paused", "startpending", "pausepending", "continuepending", "stoppending" };
-	const std::string ServiceStatusDefaultValue[7] = { "RUNNING", "STOPPED", "PAUSED", "START_PENDING", "PAUSE_PENDING", "CONTINUE_PENDING", "STOP_PENDING" };
-	for (int i = 0; i < 7; i++) StatusList.emplace(std::make_pair(ServiceStatusList[i], ini.GetString("services", ServiceStatusLoadKeyList[i], ServiceStatusDefaultValue[i])));
+	const DWORD ServiceStatusList[8] = { SERVICE_RUNNING, SERVICE_STOPPED, SERVICE_PAUSED, SERVICE_START_PENDING, SERVICE_PAUSE_PENDING, SERVICE_CONTINUE_PENDING, SERVICE_STOP_PENDING, 0 };
+	const std::string ServiceStatusLoadKeyList[8] = { "status-running", "status-stopped", "status-paused", "status-startpending", "status-pausepending", "status-continuepending", "status-stoppending", "status-unsupported" };
+	const std::string ServiceStatusDefaultValue[8] = { "RUNNING", "STOPPED", "PAUSED", "START_PENDING", "PAUSE_PENDING", "CONTINUE_PENDING", "STOP_PENDING", "Unsupported status on resource monitor server" };
+	for (int i = 0; i < 8; i++) StatusList.emplace(std::make_pair(ServiceStatusList[i], ini.GetString("services", ServiceStatusLoadKeyList[i], ServiceStatusDefaultValue[i])));
+}
+
+void ServiceMonitor::InitServiceTypeList(const IniRead& ini) {
+	const DWORD ServiceTypeList[8] = {
+		SERVICE_WIN32_OWN_PROCESS, 	SERVICE_WIN32_SHARE_PROCESS, SERVICE_KERNEL_DRIVER, SERVICE_FILE_SYSTEM_DRIVER,
+		SERVICE_ADAPTER, SERVICE_USER_OWN_PROCESS, SERVICE_USER_SHARE_PROCESS, 0
+	};
+	const std::string ServiceTypeLoadKeyList[8] = { "type-own", "type-share", "type-kernel", "type-filesys", "type-adapt", "type-userown", "type-usershare", "type-unsupported" };
+	const std::string ServiceTypeDefaultValue[8] = { "Win32 Own Process", "Win32 Share Process", "Kernel Driver", "File System", "Adapter", "User Own Process",	"User Share Process", "Unsupported type on resource monitor server" };
+	for (int i = 0; i < 8; i++) StatusList.emplace(std::make_pair(ServiceTypeList[i], ini.GetString("services", ServiceTypeLoadKeyList[i], ServiceTypeDefaultValue[i])));
 }
 
 ServiceMonitor::ServiceMonitor(const std::string& MonitorService) : ServiceController(MonitorService) {}
@@ -19,4 +31,11 @@ void ServiceMonitor::Pause() { ServiceController::Pause(); }
 
 void ServiceMonitor::Continue() { ServiceController::Continue(); }
 
-std::string ServiceMonitor::Show() { return StatusList.at(ServiceController::Show()); }
+std::string ServiceMonitor::Show() { 
+	const DWORD Code = StatusList.find(this->ServiceStatusCode) == StatusList.end() ? 0 : this->ServiceStatusCode;
+	return StatusList.at(Code);
+}
+
+void ServiceMonitor::Update() {
+	this->ServiceStatusCode = ServiceController::Show();
+}
--- Server/ServiceMonitor.hpp
+++ Server/ServiceMonitor.hpp
@@ -6,12 +6,17 @@
 class ServiceMonitor : private ServiceController {
 private:
 	static std::unordered_map<DWORD, std::string> StatusList;
+	static std::unordered_map<DWORD, std::string> ServiceTypeList;
+	std::string ServiceType;
+	std::string ServiceStatus;
+	std::string Show();
 public:
 	ServiceMonitor(const std::string& MonitorService);
 	static void InitStatusList(const IniRead& ini);
+	static void InitServiceTypeList(const IniRead& ini);
 	void Run();
 	void Stop();
 	void Pause();
 	void Continue();
-	std::string Show();
+	void Update();
 };
