ef602dd9ee42363f36c51bc53f529a33b7c527d2
AinoMegumi <aimegu2014@gmail.com>
2020-04-20 12:28:48 +0900

    コミット漏れ修正

---
+++ Server/ServiceControlManager.cpp
@@ -0,0 +1,17 @@
+﻿#include "ServiceControlManager.h"
+#include "GetErrorMessage.h"
+#include <stdexcept>
+
+ServiceControlManager::ServiceControlManager() {
+	if ((this->SCM = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS)) == NULL) {
+		throw std::runtime_error(
+			"Failed In OpenSCManager Function\n"
+			+ GetErrorMessageA()
+		);
+	}
+}
+
+ServiceControlManager::~ServiceControlManager() { CloseServiceHandle(this->SCM); }
+
+ServiceControlManager::operator const SC_HANDLE& () const noexcept { return this->SCM; }
+
---
+++ Server/ServiceControlManager.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <Windows.h>
+
+class ServiceControlManager {
+private:
+	SC_HANDLE SCM;
+public:
+	ServiceControlManager();
+	~ServiceControlManager();
+	operator const SC_HANDLE& () const noexcept;
+};
