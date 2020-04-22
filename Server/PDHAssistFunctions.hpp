48899b32f71ba159504f95367898cdeba4f1ded1
AinoMegumi <aimegu2014@gmail.com>
2020-04-22 12:29:36 +0900

    テラバイトに変換する関数を追加

--- Server/PDHAssistFunctions.hpp
+++ Server/PDHAssistFunctions.hpp
@@ -2,6 +2,7 @@
 constexpr double ByteToKiloByte(const long long& ByteVal) { return static_cast<double>(ByteVal) / 1024.0; }
 constexpr double ByteToMegaByte(const long long& ByteVal) { return ByteToKiloByte(ByteVal) / 1024.0; }
 constexpr double ByteToGigaByte(const long long& ByteVal) { return ByteToMegaByte(ByteVal) / 1024.0; }
+constexpr double ByteToTeraByte(const long long& ByteVal) { return ByteToGigaByte(ByteVal) / 1024.0; }
 constexpr double ToPercentBase(const long long& AvailOrUsage, const long long& Total) { return static_cast<double>(AvailOrUsage) * 100.0 / static_cast<double>(Total); }
 constexpr double ToPercentCheckLower(const long long& AvailOrUsage, const long long& Total) { return AvailOrUsage > 0 && static_cast<int>(ToPercentBase(AvailOrUsage, Total) * 100) == 0; }
 constexpr double ToPercentCheckUpper(const long long& AvailOrUsage, const long long& Total) { return AvailOrUsage != Total && static_cast<int>(ToPercentBase(AvailOrUsage, Total) * 100) == 10000; }
