1c12b61c8a5a30782b923200402fff2551952342
AinoMegumi <aimegu2014@gmail.com>
2020-04-22 15:22:03 +0900

    DXライブラリの文字出力クラスを追加

---
+++ LocalClient/StringManager.hpp
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "StringController.hpp"
+#include "Color.hpp"
+#include "DxLibHandle.hpp"
+
+class StringManager {
+public:
+	int StringSize;
+private:
+	StringHandle handle;
+	Color StringColor;
+public:
+	StringManager(const char* FontName, const int FontSize, const Color& StringColor)
+		: StringManager(std::string(FontName), FontSize, StringColor) {}
+	StringManager(const std::string& FontName, const int FontSize, const Color& StringColor) 
+		: StringSize(FontSize), handle(CreateFontToHandle(CharsetManager::AlignCmdLineStrType(FontName).c_str(), FontSize, -1)), StringColor(StringColor) {}
+	void Draw(const int X, const int Y, const std::string& str) const noexcept {
+		DrawStringToHandle(X, Y, CharsetManager::AlignCmdLineStrType(str).c_str(), this->StringColor.GetColorCode(), this->handle);
+	}
+	int GetLength(const std::string& str) {
+		return GetDrawStringWidth(CharsetManager::AlignCmdLineStrType(str).c_str(), static_cast<int>(std::strlen(str.c_str())));
+	}
+};
