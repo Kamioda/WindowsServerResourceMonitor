c5ebbf2a3e46f18cf0bf67e00492b738c3a05ee7
AinoMegumi <aimegu2014@gmail.com>
2020-04-22 15:17:11 +0900

    std::stringとstd:wstringを制御するためにCommandLineManagerを流用

---
+++ LocalClient/CommandLineManager.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+namespace CommandLine {
+	namespace CharArg {
+		std::vector<std::string> GetCommandLineArg(const char* lpCmdLine);
+		std::vector<std::string> GetCommandLineArg(const wchar_t* lpCmdLine);
+		std::string AlignCmdLineStrType(const std::string& str);
+		std::string AlignCmdLineStrType(const std::wstring& str);
+		std::vector<std::string> GetCommandLineArg(const std::vector<std::string>& args);
+		std::vector<std::string> GetCommandLineArg(const std::vector<std::wstring>& args);
+		typedef std::string CommandLineStringType;
+		typedef std::vector<std::string> CommandLineType;
+	}
+	namespace WCharArg {
+		std::vector<std::wstring> GetCommandLineArg(const char* lpCmdLine);
+		std::vector<std::wstring> GetCommandLineArg(const wchar_t* lpCmdLine);
+		std::wstring AlignCmdLineStrType(const std::string& str);
+		std::wstring AlignCmdLineStrType(const std::wstring& str);
+		std::vector<std::wstring> GetCommandLineArg(const std::vector<std::string>& args);
+		std::vector<std::wstring> GetCommandLineArg(const std::vector<std::wstring>& args);
+		typedef std::wstring CommandLineStringType;
+		typedef std::vector<std::wstring> CommandLineType;
+	}
+}
+
+namespace CommandLineManagerA = CommandLine::CharArg;
+namespace CommandLineManagerW = CommandLine::WCharArg;
---
+++ LocalClient/StringController.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "CommandLineManager.h"
+
+namespace CommandLineManager = CommandLineManagerA;
+#ifdef UNICODE
+namespace CharsetManager = CommandLineManagerW;
+#else
+namespace CharsetManager = CommandLineManagerA;
+#endif
