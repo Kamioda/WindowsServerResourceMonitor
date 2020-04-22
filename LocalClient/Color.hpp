4bd3b4ac96407087b1de672e711370a674fc1da5
AinoMegumi <aimegu2014@gmail.com>
2020-04-22 15:16:13 +0900

    色コードをDXライブラリの色データに変換するクラスを作成

---
+++ LocalClient/Color.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <DxLib.h>
+#include <string>
+#include <stdexcept>
+
+class Color {
+private:
+	int Red, Green, Blue;
+public:
+	Color() = default;
+	Color::Color(const int Red, const int Green, const int Blue)
+		: Red(Red), Green(Green), Blue(Blue) {}
+	Color::Color(const std::string& ColorCode) {
+		if (ColorCode.size() != 7 || '#' != ColorCode.at(0)) throw std::runtime_error("色コードが不正です。");
+		auto Convert = [&ColorCode](const size_t Start) { return std::stoi(ColorCode.substr(Start, 2), nullptr, 16); };
+		this->Red = Convert(1);
+		this->Green = Convert(3);
+		this->Blue = Convert(5);
+	}
+	unsigned int GetColorCode() const { return DxLib::GetColor(this->Red, this->Green, this->Blue); }
+};
