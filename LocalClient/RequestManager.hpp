5359ee03d4d79eae75cac7787bdbcdf51f5a60bf
AinoMegumi <aimegu2014@gmail.com>
2020-04-22 15:20:17 +0900

    httplibでは非同期ができないため、非同期っぽく見せるために実装を変更

--- LocalClient/RequestManager.hpp
+++ LocalClient/RequestManager.hpp
@@ -8,7 +8,6 @@ protected:
 	httplib::Client client;
 	long long RequestInterval;
 	std::chrono::milliseconds LastRequest;
-	picojson::object Response;
 	int ErrorCount;
 	int MaxErrorCount;
 	int LastStatus;
@@ -17,24 +16,25 @@ protected:
 	}
 public:
 	RequestManager(const std::string& Host, const int port, const long long Interval = 1000, const int ErrorMax = 5)
-		: client(Host, port), RequestInterval(Interval), LastRequest(), Response(), ErrorCount(), MaxErrorCount(ErrorMax), LastStatus(200) {}
-	picojson::object GetAll(const std::string& Path) {
+		: client(Host, port), RequestInterval(Interval), LastRequest(), ErrorCount(), MaxErrorCount(ErrorMax), LastStatus(200) {}
+	int GetAll(picojson::object& RetRes, const std::string& Path) {
 		const std::chrono::microseconds Now = GetCurrentClock();
-		if ((this->LastRequest - Now).count() < this->RequestInterval) return this->Response;
+		if (this->LastRequest.count() != 0 && (this->LastRequest - Now).count() < this->RequestInterval) return 1;
 		auto res = this->client.Get(Path.c_str());
+		if (res == nullptr) return 1;
 		this->LastStatus = res->status;
 		if (res->status != 200) {
-			if (this->LastStatus == 503) return this->Response; // 503はサービスが一時停止中にも来るのでエラーカウントしない
+			if (this->LastStatus == 503) return 1; // 503はサービスが一時停止中にも来るのでエラーカウントしない
 			this->ErrorCount++;
 			if (this->MaxErrorCount == this->ErrorCount) 
 				throw std::runtime_error("複数回にわたってエラーが発生しました。サーバーを確認して下さい。");
-			return this->Response;
+			return -1;
 		}
 		this->ErrorCount = 0;
 		picojson::value val{};
 		if (const std::string err = picojson::parse(val, res->body); !err.empty()) throw std::runtime_error(err);
-		this->Response = val.get<picojson::object>();
-		return this->Response;
+		RetRes = val.get<picojson::object>();
+		return 0;
 	}
 	void Post(const std::string& Path, const std::string& Body = std::string(), const std::string& ContentType = std::string()) {
 		this->client.Post(Path.c_str(), Body, ContentType.c_str());
