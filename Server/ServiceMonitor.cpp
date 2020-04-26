87258fb85b5f0fabba98d03ac946f1a4072ae999
AinoMegumi <aimegu2014@gmail.com>
2020-04-26 13:52:04 +0900

    配列を変更

--- Server/ServiceMonitor.cpp
+++ Server/ServiceMonitor.cpp
@@ -1,5 +1,6 @@
 ﻿#include "ServiceMonitor.hpp"
 #include "JsonObject.hpp"
+#include <array>
 
 std::unordered_map<DWORD, std::string> ServiceMonitor::StatusList;
 
@@ -7,20 +8,20 @@ std::unordered_map<DWORD, std::string> ServiceMonitor::ServiceTypeList;
 
 void ServiceMonitor::InitStatusList(const ConfigLoader& conf) {
 	constexpr int statusNum = 8;
-	const DWORD ServiceStatusList[statusNum] = { SERVICE_RUNNING, SERVICE_STOPPED, SERVICE_PAUSED, SERVICE_START_PENDING, SERVICE_PAUSE_PENDING, SERVICE_CONTINUE_PENDING, SERVICE_STOP_PENDING, 0 };
-	const std::string ServiceStatusLoadKeyList[statusNum] = { "status/running", "status/stopped", "status/paused", "status/startpending", "status/pausepending", "status/continuepending", "status/stoppending", "status/unsupported" };
-	const std::string ServiceStatusDefaultValue[statusNum] = { "RUNNING", "STOPPED", "PAUSED", "START_PENDING", "PAUSE_PENDING", "CONTINUE_PENDING", "STOP_PENDING", "Unsupported status on resource monitor server" };
+	const std::array<DWORD, statusNum> ServiceStatusList = { SERVICE_RUNNING, SERVICE_STOPPED, SERVICE_PAUSED, SERVICE_START_PENDING, SERVICE_PAUSE_PENDING, SERVICE_CONTINUE_PENDING, SERVICE_STOP_PENDING, 0 };
+	const std::array<std::string, statusNum> ServiceStatusLoadKeyList = { "status/running", "status/stopped", "status/paused", "status/startpending", "status/pausepending", "status/continuepending", "status/stoppending", "status/unsupported" };
+	const std::array<std::string, statusNum> ServiceStatusDefaultValue = { "RUNNING", "STOPPED", "PAUSED", "START_PENDING", "PAUSE_PENDING", "CONTINUE_PENDING", "STOP_PENDING", "Unsupported status on resource monitor server" };
 	for (int i = 0; i < statusNum; i++) StatusList.emplace(std::make_pair(ServiceStatusList[i], conf.GetString("services", ServiceStatusLoadKeyList[i], ServiceStatusDefaultValue[i])));
 }
 
 void ServiceMonitor::InitServiceTypeList(const ConfigLoader& conf) {
 	constexpr int typeNum = 9;
-	const DWORD ServiceTypeNumberList[typeNum] = {
+	const std::array<DWORD, typeNum> ServiceTypeNumberList = {
 		SERVICE_WIN32, SERVICE_WIN32_OWN_PROCESS, SERVICE_WIN32_SHARE_PROCESS, SERVICE_KERNEL_DRIVER, SERVICE_FILE_SYSTEM_DRIVER,
 		SERVICE_ADAPTER, SERVICE_USER_OWN_PROCESS, SERVICE_USER_SHARE_PROCESS, 0
 	};
-	const std::string ServiceTypeLoadKeyList[typeNum] = { "type/win32", "type/own", "type/share", "type/kernel", "type/filesys", "type/adapt", "type/userown", "type/usershare", "type/unsupported" };
-	const std::string ServiceTypeDefaultValue[typeNum] = { "Win32", "Win32 Own Process", "Win32 Share Process", "Kernel Driver", "File System", "Adapter", "User Own Process",	"User Share Process", "Unsupported type on resource monitor server" };
+	const std::array<std::string, typeNum> ServiceTypeLoadKeyList = { "type/win32", "type/own", "type/share", "type/kernel", "type/filesys", "type/adapt", "type/userown", "type/usershare", "type/unsupported" };
+	const std::array<std::string, typeNum> ServiceTypeDefaultValue = { "Win32", "Win32 Own Process", "Win32 Share Process", "Kernel Driver", "File System", "Adapter", "User Own Process",	"User Share Process", "Unsupported type on resource monitor server" };
 	for (int i = 0; i < typeNum; i++) StatusList.emplace(std::make_pair(ServiceTypeNumberList[i], conf.GetString("services", ServiceTypeLoadKeyList[i], ServiceTypeDefaultValue[i])));
 }
 
