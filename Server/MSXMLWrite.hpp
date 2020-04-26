fc1a4c98ab112c313a2ce630ae2262f2453eaebb
AinoMegumi <aimegu2014@gmail.com>
2020-04-27 02:14:11 +0900

    MSXMLのWriterで使うクラスとクラスそのものの名前を更新し、MSXMLのnamespaceに入れる

--- Server/MSXMLWrite.hpp
+++ Server/MSXMLWrite.hpp
@@ -1,56 +1,21 @@
 ﻿#pragma once
-#include <msxml.h>
-#include <string>
+#include "XmlDomElement.hpp"
 #include <type_traits>
 
-namespace XmlWriteEngine {
-	class XmlDomDocument {
+namespace MSXML {
+	class Write {
 	private:
-		IXMLDOMDocument* lpXmlDoc;
+		XmlDomDocument lpXmlDoc;
+		XmlDomElement lpRoot;
 	public:
-		XmlDomDocument();
-		XmlDomDocument(std::nullptr_t);
-		~XmlDomDocument();
-		XmlDomDocument(const XmlDomDocument&) = delete;
-		XmlDomDocument(XmlDomDocument&& x) noexcept;
-		XmlDomDocument& operator = (const XmlDomDocument&) = delete;
-		XmlDomDocument& operator = (XmlDomDocument&& x) noexcept;
-		IXMLDOMDocument* operator -> () noexcept;
-		IXMLDOMDocument* operator -> () const noexcept;
-		operator IXMLDOMDocument* () const noexcept;
-	};
-
-	class XmlDomElement {
-	private:
-		IXMLDOMElement* element;
-	public:
-		XmlDomElement();
-		XmlDomElement(std::nullptr_t);
-		XmlDomElement(const XmlDomDocument& lpXmlDoc, const std::wstring& key);
-		XmlDomElement(const XmlDomDocument& lpXmlDoc, const std::wstring& key, const std::wstring& data);
-		~XmlDomElement();
-		XmlDomElement(const XmlDomElement&) = delete;
-		XmlDomElement(XmlDomElement&& x) noexcept;
-		XmlDomElement& operator = (const XmlDomElement&) = delete;
-		XmlDomElement& operator = (XmlDomElement&& x) noexcept;
-		IXMLDOMElement* operator -> () noexcept;
-		IXMLDOMElement* operator -> () const noexcept;
-		operator IXMLDOMElement* () const noexcept;
+		Write(const std::wstring& Root);
+		XmlDomElement GenerateElement(const std::wstring& key);
+		XmlDomElement GenerateElement(const std::wstring& key, const std::wstring& data);
+		template<typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+		XmlDomElement GenerateElement(const std::wstring& key, const T& data) {
+			return this->GenerateElement(key, std::to_wstring(data));
+		}
+		void AddToRootElement(const XmlDomElement& elem);
+		void Output(const std::wstring& FilePath);
 	};
 }
-
-class MSXMLWrite {
-private:
-	XmlWriteEngine::XmlDomDocument lpXmlDoc;
-	XmlWriteEngine::XmlDomElement lpRoot;
-public:
-	MSXMLWrite(const std::wstring& Root);
-	XmlWriteEngine::XmlDomElement GenerateElement(const std::wstring& key);
-	XmlWriteEngine::XmlDomElement GenerateElement(const std::wstring& key, const std::wstring& data);
-	template<typename T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
-	XmlWriteEngine::XmlDomElement GenerateElement(const std::wstring& key, const T& data) {
-		return this->GenerateElement(key, std::to_wstring(data));
-	}
-	void AddToRootElement(const XmlWriteEngine::XmlDomElement& elem);
-	void Output(const std::wstring& FilePath);
-};
