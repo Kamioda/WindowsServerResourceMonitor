7921d107d5673f3503f568c9f6528442528c4439
AinoMegumi <aimegu2014@gmail.com>
2020-04-27 22:48:36 +0900

    リファクタリングとstd::wstring版の作成

--- Server/ConfigLoader.cpp
+++ Server/ConfigLoader.cpp
@@ -1,15 +1,14 @@
 ﻿#include "ConfigLoader.hpp"
-#include "../Common/CommandLineManager.h"
+#include "../Common/StringCvt.h"
 #include <Windows.h>
 constexpr size_t ArrayBufferSize = 1024;
 constexpr const wchar_t* CommonRoot = L"configuration/";
 
-ConfigLoader::ConfigLoader(const std::string& ConfigFilePath) : xml(CommandLineManagerW::AlignCmdLineStrType(ConfigFilePath)) {}
+ConfigLoader::ConfigLoader(const std::string& ConfigFilePath) : xml(string::converter::stl::from_bytes(ConfigFilePath)) {}
 
 int ConfigLoader::GetNum(const std::string& Section, const std::string& Key, const int DefaultValue) {
 	try {
-		const std::wstring TargetRoot = CommonRoot + CommandLineManagerW::AlignCmdLineStrType(Section + "/" + Key);
-		return this->xml.Get<int>(TargetRoot).front();
+		return this->xml.Get<int>(CommonRoot + string::converter::stl::from_bytes(Section + "/" + Key)).front();
 	}
 	catch (std::exception) {
 		return DefaultValue;
@@ -18,8 +17,17 @@ int ConfigLoader::GetNum(const std::string& Section, const std::string& Key, con
 
 std::string ConfigLoader::GetString(const std::string& Section, const std::string& Key, const std::string& Default) {
 	try {
-		const std::wstring TargetRoot = CommonRoot + CommandLineManagerW::AlignCmdLineStrType(Section + "/" + Key);
-		return this->xml.Get<std::string>(TargetRoot).front();
+		return this->xml.Get<std::string>(CommonRoot + string::converter::stl::from_bytes(Section + "/" + Key)).front();
+	}
+	catch (std::exception) {
+		return Default;
+	}
+
+}
+
+std::wstring ConfigLoader::GetString(const std::wstring& Section, const std::wstring& Key, const std::wstring& Default) {
+	try {
+		return this->xml.Get<std::wstring>(CommonRoot + Section + L"/" + Key).front();
 	}
 	catch (std::exception) {
 		return Default;
--- Server/ConfigLoader.hpp
+++ Server/ConfigLoader.hpp
@@ -10,4 +10,5 @@ public:
 	ConfigLoader(const std::string& ConfigFilePath);
 	int GetNum(const std::string& Section, const std::string& Key, const int DefaultValue);
 	std::string GetString(const std::string& Section, const std::string& Key, const std::string& Default);
+	std::wstring GetString(const std::wstring& Section, const std::wstring& Key, const std::wstring& Default);
 };
