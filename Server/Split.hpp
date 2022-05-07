db5f13c731765a89ef3584af2f7e506a42a0f919
AinoMegumi <aimegu2014@gmail.com>
2022-05-07 14:33:40 +0900

    Split.hppを最新版に更新

--- Server/Split.hpp
+++ Server/Split.hpp
@@ -1,9 +1,7 @@
-﻿#ifndef __SPLIT_HPP__
-#define __SPLIT_HPP__
+﻿#pragma once
 #include <string>
 #include <vector>
 #include <array>
-#include <sstream>
 #include <type_traits>
 #include <algorithm>
 
@@ -31,7 +29,7 @@ template<size_t ArrSize, typename T, std::enable_if_t<std::is_same<T, char>::val
 std::array<std::basic_string<T>, ArrSize> SplitString(const std::basic_string<T>& str, const T sep) {
 	const std::vector<std::basic_string<T>> List = SplitString<T>(str, sep);
 	if (List.size() > ArrSize) throw std::runtime_error("Split data is over.");
-	std::array<std::basic_string<T>, ArrSize> RetData;
+	std::array<std::basic_string<T>, ArrSize> RetData{};
 	RetData.fill(T{});
 	for (size_t i = 0; i < std::min(ArrSize, List.size()); i++) RetData[i] = std::move(List[i]);
 	return RetData;
@@ -40,5 +38,56 @@ std::array<std::basic_string<T>, ArrSize> SplitString(const std::basic_string<T>
 template<size_t ArrSize, typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
 std::array<std::basic_string<T>, ArrSize> SplitString(const T*& str, const T sep) {
 	return SplitString(std::basic_string<T>(str), sep);
+}
+
+#if _HAS_CXX17
+#include <string_view>
+
+#if _HAS_CXX20
+#include <utility>
+
+template<typename T>
+inline T exchange(T& data, T& newVal) {
+	return std::exchange(data, newVal);
+}
+#else
+template<typename T>
+inline T exchange(T& data, T& newVal) {
+	const T ret = data;
+	data = newVal;
+	return ret;
+}
+#endif
+
+template<typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::vector<std::basic_string_view<T>> SplitString(const std::basic_string_view<T>& str, const T sep) {
+	std::vector<std::basic_string_view<T>> Arr{};
+	size_t start = 0, last = 0;
+	for (const T& i : str) {
+		if (i == sep) Arr.push_back(std::string_view(str.begin() + exchange(start, last + 1), str.begin() + last));
+		last++;
+	}
+	if (last > start) Arr.push_back(std::string_view(str.begin() + exchange(start, last), str.begin() + last));
+	return Arr;
+}
+
+template<typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::vector<std::basic_string_view<T>> SplitString(const T* str, const T sep) {
+	return SplitString(std::basic_string_view<T>(str), sep);
+}
+
+template<size_t ArrSize, typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::array<std::basic_string_view<T>, ArrSize> SplitString(const std::basic_string_view<T>& str, const T sep) {
+	const std::vector<std::basic_string_view<T>> List = SplitString<T>(str, sep);
+	if (List.size() > ArrSize) throw std::runtime_error("Split data is over.");
+	std::array<std::basic_string_view<T>, ArrSize> RetData{};
+	RetData.fill(T{});
+	for (size_t i = 0; i < std::min(ArrSize, List.size()); i++) RetData[i] = std::move(List[i]);
+	return RetData;
+}
+
+template<size_t ArrSize, typename T, std::enable_if_t<std::is_same<T, char>::value || std::is_same<T, wchar_t>::value, std::nullptr_t> = nullptr>
+std::array<std::basic_string_view<T>, ArrSize> SplitString(const T*& str, const T sep) {
+	return SplitString(std::basic_string_view<T>(str), sep);
 }
 #endif
