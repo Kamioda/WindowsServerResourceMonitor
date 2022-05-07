f4e82505980203f231149233da1ce5b011c89e28
AinoMegumi <aimegu2014@gmail.com>
2022-05-07 12:21:36 +0900

    不要になったメンバー関数宣言を削除

--- Server/ResourceAccessServer.hpp
+++ Server/ResourceAccessServer.hpp
@@ -12,9 +12,6 @@ private:
 	ResourceMonitor resource;
 	AuthManager auth;
 	using BaseClass = ServiceProcess;
-	std::string GetConfStr(const std::string& Section, const std::string& Key, const std::string& Default);
-	std::wstring GetConfStr(const std::wstring& Section, const std::wstring& Key, const std::wstring& Default);
-	int GetConfInt(const std::string& Section, const std::string& Key, const int& Default);
 public:
 	ResourceAccessServer(const Service_CommandLineManager::CommandLineType& args);
 	void Service_MainProcess() override;
