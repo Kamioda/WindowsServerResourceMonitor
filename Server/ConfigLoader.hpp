47ee6a3b9cebef34b119b92cc06e61d23cce86c3
AinoMegumi <aimegu2014@gmail.com>
2022-05-08 20:07:14 +0900

    ConfigLoaderをヘッダーオンリーに変更

--- Server/ConfigLoader.hpp
+++ Server/ConfigLoader.hpp
@@ -1,7 +1,10 @@
 ﻿#pragma once
 #include "../Common/MSXMLRead.hpp"
+#include "../Common/StringCvt.h"
 #include "json.hpp"
 #include <string>
+#include <type_traits>
+#include <fstream>
 #include <filesystem>
 
 class ConfigLoader {
@@ -10,13 +13,40 @@ private:
 public:
 	ConfigLoader() = default;
 	ConfigLoader(const std::string& ConfigFilePath);
-	int GetNum(const std::string& route, const int& DefaultValue);
-	int GetNum(const std::wstring& route, const int& DefaultValue);
-	std::string GetString(const std::string& route, const std::string& Default);
+	template<std::integral T>
+	T GetNum(const std::string& route, const T& DefaultValue) {
+		try {
+			return this->xml.Get<int>(string::converter::stl::from_bytes(route)).front();
+		}
+		catch (std::exception) {
+			return DefaultValue;
+		}
+	}
+	template<std::integral T>
+	int GetNum(const std::wstring& route, const int& DefaultValue) {
+		try {
+			return this->xml.Get<int>(route).front();
+		}
+		catch (std::exception) {
+			return DefaultValue;
+		}
+	}
+	std::string GetString(const std::string& route, const std::string& Default) {
+
+	}
 	std::wstring GetString(const std::wstring& route, const std::wstring& Default);
 private:
-	static nlohmann::json GetJson(const std::filesystem::path& JsonFile);
+	static nlohmann::json GetJson(const std::filesystem::path& JsonFile) {
+		if (std::filesystem::status(JsonFile).type() != std::filesystem::file_type::regular)
+			throw std::runtime_error(JsonFile.string() + " : File is not found.");
+		std::ifstream ifs(JsonFile);
+		return nlohmann::json::parse(std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>()));
+	}
 public:
-	nlohmann::json GetJson(const std::string& route, const std::string& DefaultFile);
-	nlohmann::json GetJson(const std::wstring& route, const std::wstring& DefaultFile);
+	nlohmann::json GetJson(const std::string& route, const std::string& DefaultFile) {
+		return GetJson(this->GetString(route, DefaultFile));
+	}
+	nlohmann::json GetJson(const std::wstring& route, const std::wstring& DefaultFile) {
+		return GetJson(this->GetString(route, DefaultFile));
+	}
 };
